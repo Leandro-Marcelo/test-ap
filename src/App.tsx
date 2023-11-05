26563e26b675f8c2af831a43bbcd55f59411584d
Leandro Marcelo <leandro.supanta@mercadolibre.cl>
2023-11-05 00:02:36 -0300

    deploy

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div className='w-full h-screen flex justify-between items-center flex-col'>
+        <div className='w-[200px] h-[200px]'>
+        <div>Libro que me recomendó cherry_3yes</div>
+        <img src="https://images-na.ssl-images-amazon.com/images/S/compressed.photo.goodreads.com/books/1633924377i/59117392.jpg" alt="" />
+        <div>OJITO 👁️ Creado por Lean</div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
